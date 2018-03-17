1573160f605adc97268dd32cfe9f7245ed4082f6
Frank Tianyu Zeng <tianyuzeng1997@gmail.com>
2018-03-17 12:45:22 -0500

    Use tidyquant instead of quantmod

--- retrieve_data.R
+++ retrieve_data.R
@@ -1,6 +1,6 @@
 library(twitteR)
-library(quantmod)
 library(tidyverse)
+library(tidyquant)  
 
 consumer_key <- "z0f8WnP1dGcAogS2o7GKyp2Ui"
 consumer_secret <- "pzeAdwLUkrXZvZx41ex2iSrDqMyXgCI7CIqMDPCyNrucG1wqto"
@@ -16,7 +16,8 @@ trumpTw = userTl %>% twListToDF() %>% as.tibble()
 trumpTw %>% filter(str_detect(text,'guns|shooting')) %>% select(text,created)
 
 
-chartSeries(AOBC, TA=NULL)
-getSymbols("AOBC")
-aobc = data.frame(date=index(AOBC), coredata(AOBC)) %>% as.tibble()
-aobc
+gunstock = tq_get("AOBC", get = "stock.prices", from = "2015-09-01", to = "2018-2-10")
+gunstock %>% 
+  ggplot(aes(x = date, y = close)) +
+  geom_line() +
+  labs(title = "AOBC Line Chart", y = "Closing Price", x = "") 
