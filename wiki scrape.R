5ec7d81c66e8665a2fb280bb2821cca0183c3668
bennettmajer <35977336+bennettmajer@users.noreply.github.com>
2018-03-22 13:26:42 -0500

    update

--- wiki scrape.R
+++ wiki scrape.R
@@ -1,115 +1,67 @@
 ###### my code
 
-library("rvest")
+
 library(tidyverse)
+library(data.table)
 rm(list = ls())
 setwd("~/Documents/Stat 479")
 counties = "https://en.wikipedia.org/wiki/List_of_United_States_counties_and_county_equivalents"
+
+# URLs for all county wikipedia pages
 lines = readLines(counties) # thousands of lines of HTML
 pop = grep(pattern = "text-align:right", x = lines)
 county.lines = lines[pop-2]
 county.lines = gsub("^.*(/wiki/)", "", county.lines)
 county.lines = gsub("[[:space:]]", replacement = "", x = county.lines)
 county.lines = gsub("\\\".*$", replacement = "", x = county.lines)
-county.urls = paste("/wiki/", county.lines, sep="")
+county.urls = paste("https://en.wikipedia.org/wiki/", county.lines, sep="")
 
-county.sites = paste("https://en.wikipedia.org", county.urls, sep="")
-
-
-
-### change to full data
-county.urls = county.urls[1:20]
-
-counties.df = data_frame(county = 1:length(county.urls), links = 0)
-for (i in 1:length(county.urls)) {
-  temp.html = readLines(paste("https://en.wikipedia.org", county.urls[i], sep=""))
-  temp.links = grep(pattern = "<a href=", value = TRUE, x = temp.html)
-  temp.links = gsub("^.*a href=", "", temp.links)
-  temp.links = gsub(" title.*$", "", temp.links)
-  for (i in 1:length(county.urls)) {
-    if (temp.links[i] == county.urls[i]) {
-      links = paste(links, " ", temp.links, " ", sep = "")
-    }
+# scrape general county links
+scrape.links = function(url) {
+  html = readLines(url)
+  links = grep(pattern = "href=\\\"/wiki/", x = html, value = TRUE)
+  links = grep(pattern = ",_", x = links, value = TRUE)
+  c = gsub("^.*(/wiki/)", "", links)
+  c = gsub(" ", replacement = "", x = c)
+  c = gsub("\\\".*$", replacement = "", x = c)
+  index = grep(pattern = "Area", x = c)
+  c = c[-index]
+  com = grep(pattern = ",_", x = c)
+  c = c[com]
+  lower = grep(pattern = "area", x = c)
+  if (length(lower) > 0) {
+    c = c[-lower]
   }
-  counties.df$links[i] = links
-}
-
-####### inside loop
-# 1. read in html for all counties
-# 2. narrow down to just county links
-# 3. put those links in something else
-
-####### outside loop
-# 4. compare links in new list to links in old list
-# 5. sparse matrix based on county link graph
-
-# 6. interpret one of the leading eigenvectors of sparse matrix
-
-
-
-temp.links
-
-newlines = readLines(county.sites)
-
-find.links = function(urls) {
-  county = readLines()
-  
-  
-  for (1:length(urls)) {
-    newlines = readLines(paste("https://en.wikipedia.org", urls[i], sep=""))
-    county.linklines[i] = grep(pattern = urls
+  colons = grep(pattern = ":", x = c)
+  if (length(colons) > 0) {
+    c = c[-colons]
   }
-  lines = readLines(urls)
-  county.linklines = grep(pattern = urls, lines)
-  county.linklines
+  paste("https://en.wikipedia.org/wiki/", c, sep="")
 }
 
 
-
-######## karl's trump code
-library(data.table)
-library(tidyverse)
-library(tidytext)
-library(Matrix)
-# install.packages("glmnet")
-library(glmnet)
-library(randomForest)
+test = county.urls[1:9]
+t = gsub("^.*/wiki/", "", test)
+t = gsub("_", " ", t)
+tib = as_data_frame(cbind(t, test))
+a = apply(tib[2], 1, scrape.links)
 
 
-data = fread("https://raw.githubusercontent.com/bpb27/political_twitter_archive/master/realdonaldtrump/realdonaldtrump.csv") %>% as.tbl
-str(data)
-
-text_df <- data_frame(tweet = 1:nrow(data), text = data$text)
-text_df$text
-
-# this does a lot of processing! 
-#  to lower, remove @ # , . 
-#  often these make sense on a first cut.
-#  worth revisiting before "final results"!
-tt  = text_df %>% unnest_tokens(word, text)
-str(tt)
-
-# make the document-term matrix.  
-#   I sometimes call this the bag-of-words-matrix.
-dt = cast_sparse(tt, tweet, word)
-str(dt)
-dim(dt)
-hist(rowSums(dt))
-cs = colSums(dt)
-hist(log(cs[cs>1]))
+j = unlist(a)
+fj = factor(j)
+p = c(0,cumsum(unlist(lapply(a, length))))
+A = sparseMatrix(j = as.numeric(fj), p = p, x = rep(1, length(fj)))
+dim(A)
+colnames(A) = levels(fj)
+rownames(A) = tib$t
 
 
-# let's take the svd to make clusters/topics 
+
 library(rARPACK)
-A = dt; Dl = Diagonal(nrow(A), 1/sqrt(rowSums(A)+10)); Dr = Diagonal(ncol(A), 1/sqrt(colSums(A)+10))
+Dl = Diagonal(nrow(A), 1/sqrt(rowSums(A)+10)); Dr = Diagonal(ncol(A), 1/sqrt(colSums(A)+10))
 L = Dl%*%A%*%Dr
 s = svds(L, k = 10)
+
 plot(s$d[-1])
 u = s$u 
-
 plot(as.data.frame(u[sample(nrow(A),1000),]), pch = ".")
-
-text_df[which(s$u[,2]< -.01),]$text
-colnames(dt)[which(s$v[,2]> .1)]
-
-
